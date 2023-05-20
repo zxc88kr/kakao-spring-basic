4529a356b8f60fb7f758fae12cb31d8e1faaa82f
zxc88kr <zxc88kr@naver.com>
2023-05-20 16:12:33 +0900

    Spring02

---
+++ Spring02/src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -0,0 +1,37 @@
+package com.fastcampus.ch2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class YoilTeller {
+	@RequestMapping("/getYoil")
+	public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String year = request.getParameter("year");
+		String month = request.getParameter("month");
+		String day = request.getParameter("day");
+		
+		int yyyy = Integer.parseInt(year);
+		int mm = Integer.parseInt(month);
+		int dd = Integer.parseInt(day);
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(yyyy, mm - 1, dd);
+		
+		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+		char yoil = " 일월화수목금토".charAt(dayOfWeek);
+		
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		
+		out.println(year + "년 " + month + "월, " + day + "일은");
+		out.println(yoil + "요일입니다.");
+	}
+}
