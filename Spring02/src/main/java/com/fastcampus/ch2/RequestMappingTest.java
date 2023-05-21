9f43a8e5a7c26dd177f748d3ec3c07cdf30c415b
zxc88kr <zxc88kr@naver.com>
2023-05-21 19:00:14 +0900

    Spring02 RequestMapping

---
+++ Spring02/src/main/java/com/fastcampus/ch2/RequestMappingTest.java
@@ -0,0 +1,37 @@
+package com.fastcampus.ch2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RequestMappingTest {
+	@RequestMapping("/login/hello.do")
+	public void test1() {
+		System.out.println("urlpattern=/login/hello.do");
+	}
+
+	@RequestMapping("/login/*")
+	public void test2() {
+		System.out.println("urlpattern=/login/*");
+	}
+
+	@RequestMapping("/login/**/tmp/*.do")
+	public void test3() {
+		System.out.println("urlpattern=/login/**/tmp/*.do");
+	}
+
+	@RequestMapping("/login/??")
+	public void test4() {
+		System.out.println("urlpattern=/login/??");
+	}
+
+	@RequestMapping("*.do")
+	public void test5() {
+		System.out.println("urlpattern=*.do");
+	}
+
+	@RequestMapping("/*.???")
+	public void test6() {
+		System.out.println("urlpattern=*.???");
+	}
+}
