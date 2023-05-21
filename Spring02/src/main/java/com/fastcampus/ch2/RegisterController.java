3b2ef9fb3289696e9f9a8a18477b7f7e7c14de48
zxc88kr <zxc88kr@naver.com>
2023-05-21 20:19:15 +0900

    Spring02 Redirect To Forward

--- Spring02/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ Spring02/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -4,28 +4,30 @@ import java.net.URLEncoder;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RegisterController {
-	@GetMapping("/register/add")
+//	@GetMapping("/register/add")
+	@RequestMapping(value="/register/add", method={RequestMethod.GET, RequestMethod.POST})
 	public String add() {
 		return "registerForm";
 	}
 
-	@PostMapping("/register/save")
+//	@PostMapping("/register/save")
+	@RequestMapping(value="/register/save", method=RequestMethod.POST)
 	public String save(User user, Model model) throws Exception {
 		if (!isValid(user)) {
 			String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
 			
 			model.addAttribute("msg", msg);
-			return "redirect:/register/add";
+			return "forward:/register/add";
 		}
 		return "registerInfo";
 	}
 
 	private boolean isValid(User user) {
-		return true;
+		return false;
 	}
 }
